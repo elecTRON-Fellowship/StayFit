cdabf528872bf116794e818d755e70f6d767e24c
amansharma2910 <amansharma2910@gmail.com>
2021-06-23 16:02:59 +0530

    Refactor code

--- stay_fit/lib/main.dart
+++ stay_fit/lib/main.dart
@@ -1,16 +1,21 @@
+import 'dart:async';
+import 'package:camera/camera.dart';
+
 import 'package:flutter/material.dart';
-import 'package:flutter_login_attempt/screens/home_page.dart';
-import 'package:flutter_login_attempt/screens/login_screen.dart';
-import 'package:flutter_login_attempt/screens/yoga_screen.dart';
+
 import 'screens/options_screen.dart';
-import 'screens/login_screen.dart';
-import 'screens/home_page.dart';
 import 'screens/profile_screen.dart';
 import 'screens/wallet_screen.dart';
 import 'screens/yoga_screen.dart';
 import 'screens/meditation_screen.dart';
 
-void main() => runApp(StayFit());
+late List<CameraDescription> cameras;
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  cameras = await availableCameras();
+  runApp(StayFit());
+}
 
 class StayFit extends StatelessWidget {
   @override
@@ -21,13 +26,14 @@ class StayFit extends StatelessWidget {
       theme: ThemeData(
         primaryColor: Colors.black,
       ),
+      initialRoute: '/options',
       routes: {
-        '/': (context) => HomePage(),
+        // '/': (context) => HomePage(),
         '/options': (context) => OptionsScreen(),
         '/profile': (context) => ProfileScreen(),
         '/wallet': (context) => WalletScreen(),
         '/meditation': (context) => MeditationScreen(),
-        '/yoga': (context) => YogaScreen(),
+        '/yoga': (context) => YogaScreen(cameras),
       },
     );
   }
